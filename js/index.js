2e4833264a834ac1e190659ca84286b50197f92a
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-06-17 13:26:45 +0500

    men osha

---
+++ js/index.js
@@ -0,0 +1,445 @@
+// Funksiyaga oid masalalar.
+
+// masala 1
+// function yegindi(arg1, arg2){
+//     let res = arg1+arg2;
+//     return res;
+// }
+// console.log(yegindi(2,3))
+
+
+
+// masala 2
+// function yegindi(arg1, arg2){
+//     let res = arg1*arg2;
+//     return res;
+// }
+// console.log(yegindi(3,6))
+
+
+// masala 3
+// function yegindi(arg1){
+//     let res = arg1**2;
+//     return res;
+// }
+// console.log(yegindi(3))
+
+
+// masala 4
+// function yegindi(arg1){
+//     let res = arg1**3;
+//     return res;
+// }
+// console.log(yegindi(3))
+
+
+// masala 5
+// function yegindi(arg1, arg2, arg3){
+//     let res = (arg1+arg2+arg3)/3;
+//     return res;
+// }
+// console.log(yegindi(3,10,2))
+
+// masala 6
+// function yegindi(arg1){
+//     let res = "";
+//     if(arg1%2===0){
+//         res = true
+//     }else{
+//         res = false
+//     }
+//     return res;
+// }
+// console.log(yegindi(3))
+
+
+// masala 7
+// function yegindi(arg1){
+//     let res = "";
+//     if(arg1%2===){
+//         res = true
+//     }else{
+//         res = false
+//     }
+//     return res;
+// }
+// console.log(yegindi(3))
+
+
+// masala 8
+// function yegindi(arg1, arg2){
+//     let res = "";
+//     if(arg1>arg2){
+//         res = arg1
+//     }else{
+//         res = arg2
+//     }
+//     return res;
+// }
+// console.log(yegindi(3,1))
+
+
+// masala 9
+// function yegindi(arg1, arg2){
+//     let res = "";
+//     if(arg1>arg2){
+//         res = arg2
+//     }else{
+//         res = arg1
+//     }
+//     return res;
+// }
+// console.log(yegindi(3,1))
+
+
+// masala 10
+// function yegindi(arg){
+//     let y = 0;
+//     for(let i = 1; i<=arg; i++){
+//         y+=i;
+
+//     }
+//     return y;
+// }
+// console.log(yegindi(5))
+
+// masala 11
+// function yegindi(arg){
+//     let y = 1;
+//     for(let i = 1; i<=arg; i++){
+//         y*=i;
+
+//     }
+//     return y;
+// }
+// console.log(yegindi(5))
+
+// masala 12
+// function age (yourAge){
+//     let res='';
+//     if(yourAge>=18){
+//         res ="katta"
+
+//     }else{
+//         res ="kichik"
+
+//     }
+// return res;
+// }
+// console.log( age(20))
+
+
+// masala 13
+// function yegindi (arg1, arg2){
+//     let res='';
+//     if(arg1%arg2===0){
+//         res ="true"
+
+//     }else{
+//         res ="false"
+
+//     }
+// return res;
+// }
+// console.log( yegindi(20,3))
+
+// masala 14
+// function yegindi (arg1){
+//     let res='';
+//     if(arg1>0){
+//         res ="true"
+
+//     }else{
+//         res ="false"
+
+//     }
+// return res;
+// }
+// console.log( yegindi(8))
+
+// masala 15
+// function yegindi (arg1){
+//     let res='';
+//     if(arg1<0){
+//         res ="true"
+
+//     }else{
+//         res ="false"
+
+//     }
+// return res;
+// }
+// console.log( yegindi(8))
+
+
+
+// masala 16
+// function yegindi (arg1){
+//     let res=arg1*1/2;
+    
+// return res;
+// }
+// console.log( yegindi(8))
+
+
+// masala 17
+// function kvadrad(ag1, ag2){
+//     let res ='';
+//     if(ag1>ag2){
+//         res=ag1-ag2;
+//     } else if (ag1<ag2){
+//         res = ag2-ag1;
+//     } else{
+//         res = 'farq yo`q'
+//     }
+//     return res;
+// }
+// console.log(kvadrad(12,10))
+
+
+// masala 18
+// function yegindi (arg1, arg2, arg3){
+//     let res='';
+//     if(arg1>arg2){
+//         res =arg1;
+
+//     }else{
+//         res =arg2;
+
+//     }
+//     if(res<arg3){
+//         res = arg3;
+//     }
+// return res;
+// }
+// console.log( yegindi(8,5,1))
+
+
+// masala 19
+// function yegindi (arg1, arg2, arg3){
+//     let res='';
+//     if(arg1<arg2){
+//         res =arg1;
+
+//     }else{
+//         res =arg2;
+
+//     }
+//     if(res>arg3){
+//         res = arg3;
+//     }
+// return res;
+// }
+// console.log( yegindi(8,5,1))
+
+// masala 20
+// function yegindi (arg1){
+//     let res='';
+//     if(arg1>10){
+//         res ="Katta";
+
+//     }else{
+//         res ="Kichik";
+
+//     }
+  
+// return res;
+// }
+// console.log( yegindi(8))
+
+
+// masala 21
+// function yegindi (arg1, arg2){
+//     let yeg = arg1+arg2;
+//     let res='';
+//     if(yeg%2===0){
+//         res ="true";
+
+//     }else{
+//         res ="false";
+
+//     }
+    
+// return res;
+// }
+// console.log( yegindi(8,5))
+
+
+// masala 22
+// function yegindi (arg1){
+
+//     let res='';
+//     if(arg1%5===0){
+//         res ="true";
+
+//     }else{
+//         res ="false";
+//     }
+    
+// return res;
+// }
+// console.log( yegindi(8))
+
+
+// masala 23
+// function yegindi (arg1, arg2){
+
+//     let res='';
+//     if(arg1>arg2){
+//         res ="true";
+
+//     }else{
+//         res ="false";
+
+//     }
+    
+// return res;
+// }
+// console.log( yegindi(8,5))
+
+
+// masala 24
+// function yegindi (arg1){
+
+//     let res='';
+//     if(arg1%7===0){
+//         res ="true";
+
+//     }else{
+//         res ="false";
+
+//     }
+    
+// return res;
+// }
+// console.log( yegindi(8))
+
+
+// masala 25
+// function daraja (arg1){
+//     let res = arg1**10;
+//     return res;
+// }
+// console.log(daraja(5))
+
+
+// masala 26
+// function orta (ag1,ag2){
+//     let res = (ag1+ag2)/2;
+//     return res;
+// }
+// console.log(orta(20,26))
+
+
+// masala 27
+// function orta (ag1){
+//     let res ="";
+//     if (ag1<100){
+//         res = "Kichik";
+//     }else{
+//         res = "Katta";
+
+//     }
+//     return res;
+// }
+// console.log(orta(20))
+
+
+// masala 28
+// function orta (ag1){
+//     let res ="";
+//     if (ag1%2===0){
+//         res = "treu";
+//     }else{
+//         res = "false";
+
+//     }
+//     return res;
+// }
+// console.log(orta(20))
+
+// masala 29
+// function orta (ag1, ag2){
+//     let res ="";
+//     let p = ag1*ag2;
+//     res = p*1/2
+
+//     return res;
+// }
+// console.log(orta(20,3))
+
+
+// masala 30
+// function orta (ag1, ag2, ag3){
+//     let res ="";
+//     if (ag1===ag2+ag3){
+//         res = "true";
+//     }else{
+//         res = "false";
+
+//     }
+//     return res;
+// }
+// console.log(orta(20,10,10))
+
+
+
+// For yordamida ishlanadigan masalalar
+// masala 1
+// function yegindi(ag1=10){
+//  let y =0;
+// for(let i=1; i<=ag1; i++){
+//     y+=i;
+
+// }
+// return y;
+// }
+// console.log(yegindi())
+
+// masala 2
+// function yegindi(ag1=5){
+//  let p =1;
+// for(let i=1; i<=ag1; i++){
+//     p*=i;
+
+// }
+// return p;
+// }
+// console.log(yegindi())
+
+// masala 3
+// function yegindi(ag1=10){
+//  let f = 1;
+// for(let i=1; i<=ag1; i++){
+//     f*=i;
+
+// }
+// return f;
+// }
+
+// console.log(yegindi())
+
+
+// masala 4
+// function number (ag1){
+//     let res ='';
+//     for(let i=1; i<=ag1; i++){
+//         if(i%2===0){
+//             res =console.log(i);
+//         }
+//     }
+//     return res;
+// }
+// console.log(number(10))
+
+// masala 5
+// function number (ag1){
+//     let res ='';
+//     for(let i=1; i<=ag1; i++){
+//         if(i%2===1){
+//             res =console.log(i);
+//         }
+//     }
+//     return res;
+// }
+// console.log(number(10))
